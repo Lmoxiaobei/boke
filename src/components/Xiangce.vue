8b6114913feb95353e66defd5c5a31ce76d8edf7
Lmoxiaobei <you@example.com>
2018-05-16 09:30:33 -0400

    2

--- src/components/Xiangce.vue
+++ src/components/Xiangce.vue
@@ -53,13 +53,16 @@ body {
   margin: 0;
   background: #d8f0ea;
 }
-h1, p, span {
+h1,
+p,
+span {
   font-family: monospace;
   font-weight: 300;
   text-align: center;
   color: rgba(0, 0, 0, 0.5);
 }
-h1, h1 + p {
+h1,
+h1 + p {
   margin: 2rem 1rem;
 }
 .wrapper {
@@ -71,36 +74,32 @@ h1, h1 + p {
   display: flex;
   -webkit-box-align: center;
   -webkit-align-items: center;
-      -ms-flex-align: center;
-          align-items: center;
+  -ms-flex-align: center;
+  align-items: center;
   -webkit-box-pack: center;
   -webkit-justify-content: center;
-      -ms-flex-pack: center;
-          justify-content: center;
+  -ms-flex-pack: center;
+  justify-content: center;
   margin-bottom: 3rem;
 }
 .box {
   width: 15rem;
   height: 20rem;
   padding: 0 2rem 3rem;
-  -webkit-transition: opacity 0.5s linear 0.3s,
-    -webkit-transform 0.3s linear 0s,
+  -webkit-transition: opacity 0.5s linear 0.3s, -webkit-transform 0.3s linear 0s,
     -webkit-filter 0.5s linear 0.3s;
-  transition: opacity 0.5s linear 0.3s,
-    -webkit-transform 0.3s linear 0s,
+  transition: opacity 0.5s linear 0.3s, -webkit-transform 0.3s linear 0s,
     -webkit-filter 0.5s linear 0.3s;
-  transition: transform 0.3s linear 0s,
-    filter 0.5s linear 0.3s,
+  transition: transform 0.3s linear 0s, filter 0.5s linear 0.3s,
     opacity 0.5s linear 0.3s;
-  transition:
-    transform 0.3s linear 0s,
-    filter 0.5s linear 0.3s,
-    opacity 0.5s linear 0.3s,
-    -webkit-transform 0.3s linear 0s,
+  transition: transform 0.3s linear 0s, filter 0.5s linear 0.3s,
+    opacity 0.5s linear 0.3s, -webkit-transform 0.3s linear 0s,
     -webkit-filter 0.5s linear 0.3s;
   /*transform-origin: top center;*/
 }
-.productq,.productw,.producte {
+.productq,
+.productw,
+.producte {
   position: relative;
   width: 100%;
   height: 100%;
@@ -111,24 +110,21 @@ h1, h1 + p {
   background-size: 80%;
   background-repeat: no-repeat;
   box-shadow: 0 0.1rem 0.2rem rgba(0, 0, 0, 0.1);
-  -webkit-transition: box-shadow 0.5s linear,
-    height 0.1s linear 0s;
-  transition:
-    box-shadow 0.5s linear,
-    height 0.1s linear 0s;
+  -webkit-transition: box-shadow 0.5s linear, height 0.1s linear 0s;
+  transition: box-shadow 0.5s linear, height 0.1s linear 0s;
 }
-.productw{
-    background-image: url(../assets/2.jpg);
+.productw {
+  background-image: url(../assets/2.jpg);
 }
-.producte{
-    background-image: url(../assets/3.jpg);
+.producte {
+  background-image: url(../assets/3.jpg);
 }
 .name {
   display: block;
   padding: 1rem 0.5rem;
 }
 .description {
-    font-size:20px;
+  font-size: 20px;
   position: absolute;
   bottom: 1rem;
   left: 0;
@@ -139,38 +135,29 @@ h1, h1 + p {
   -webkit-transition: opacity 0.1s linear 0s;
   transition: opacity 0.1s linear 0s;
 }
-.wrapper:hover .box:not(:hover)  {
+.wrapper:hover .box:not(:hover) {
   -webkit-filter: blur(3px);
-          filter: blur(3px);
+  filter: blur(3px);
   opacity: 0.5;
 }
-.box:hover  {
+.box:hover {
   -webkit-transform: scale(1.1);
-          transform: scale(1.1);
-  -webkit-transition: opacity 0.1s linear 0s,
-    -webkit-transform 0.3s linear 0.3s,
+  transform: scale(1.1);
+  -webkit-transition: opacity 0.1s linear 0s, -webkit-transform 0.3s linear 0.3s,
     -webkit-filter 0.1s linear 0s;
-  transition: opacity 0.1s linear 0s,
-    -webkit-transform 0.3s linear 0.3s,
+  transition: opacity 0.1s linear 0s, -webkit-transform 0.3s linear 0.3s,
     -webkit-filter 0.1s linear 0s;
-  transition: transform 0.3s linear 0.3s,
-    filter 0.1s linear 0s,
+  transition: transform 0.3s linear 0.3s, filter 0.1s linear 0s,
     opacity 0.1s linear 0s;
-  transition:
-    transform 0.3s linear 0.3s,
-    filter 0.1s linear 0s,
-    opacity 0.1s linear 0s,
-    -webkit-transform 0.3s linear 0.3s,
+  transition: transform 0.3s linear 0.3s, filter 0.1s linear 0s,
+    opacity 0.1s linear 0s, -webkit-transform 0.3s linear 0.3s,
     -webkit-filter 0.1s linear 0s;
 }
 .box:hover .product {
   height: 23rem;
   box-shadow: 0 0 1rem rgba(0, 0, 0, 0.2);
-  -webkit-transition: box-shadow 1s linear,
-    height 0.3s linear 0.5s;
-  transition:
-    box-shadow 1s linear,
-    height 0.3s linear 0.5s;
+  -webkit-transition: box-shadow 1s linear, height 0.3s linear 0.5s;
+  transition: box-shadow 1s linear, height 0.3s linear 0.5s;
 }
 .box:hover .description {
   opacity: 1;
